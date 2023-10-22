59665c2d6b926c738990bd1c5ed70480c95a74d1
ko2ic <ko2ic.dev@gmail.com>
2023-10-22 22:02:20 +0900

    Enebled OS12 or higher

---
+++ app/src/main/java/com/ko2ic/spike/glance/App.kt
@@ -0,0 +1,21 @@
+package com.ko2ic.spike.glance
+
+import android.app.Application
+import android.content.ComponentName
+import android.content.pm.PackageManager
+import android.os.Build
+
+class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val pm = applicationContext.packageManager
+            val componentName = ComponentName("com.ko2ic.spike.glance", "com.ko2ic.spike.glance.MyGlanceAppWidgetReceiver")
+            pm.setComponentEnabledSetting(
+                componentName,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+                PackageManager.DONT_KILL_APP
+            )
+        }
+    }
+}
